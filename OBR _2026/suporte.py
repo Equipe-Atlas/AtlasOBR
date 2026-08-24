5e49eefc4c3fbed3309511d86bfc01bcff54393e
JoaquimCaetanoCarvalho <joaquim.caetano460@gmail.com>
2026-08-24 20:41:22 -0300

    atualização de ler verde e ler vermelho

--- OBR _2026/suporte.py
+++ OBR _2026/suporte.py
@@ -1,55 +1,23 @@
 from pybricks.hubs import PrimeHub
-from pybricks.pupdevices import Motor, UltrasonicSensor, ColorSensor 
-from pybricks.parameters import Color, Port, Direction
+from pybricks.pupdevices import Motor, UltrasonicSensor, ColorSensor
+from pybricks.parameters import Color, Port
 from pybricks.tools import wait, StopWatch
-from pybricks.robotics import DriveBase
 
-hub = PrimeHub(broadcast_channel=1, observe_channels=[2])
+hub = PrimeHub(broadcast_channel=2, observe_channels=[1])
+hub.light.on(Color.MAGENTA)
 
-ultra = UltrasonicSensor(Port.C)
-cordir = ColorSensor(Port.B)
-cormeio = ColorSensor(Port.A)
-coresq = ColorSensor(Port.D)
-
-motor_esq = Motor(Port.F, positive_direction=Direction.COUNTERCLOCKWISE)
-motor_dir = Motor(Port.E)
-
-andar = DriveBase(motor_esq, motor_dir, 63, 133)
-andar.settings(straight_speed=100, straight_acceleration=300, turn_rate=100, turn_acceleration=300)
+cores = ColorSensor(Port.F)
 
 Color.SILVER = Color(h=0, s=0, v=75)
-Color.BLACK = Color(h=240 < 170, s=40<1, v= 100 < 10)
-cores = (Color.GREEN, Color.SILVER, Color.BLACK, Color.WHITE, Color.NONE, Color.RED)
-cordir.detectable_colors(cores)
-coresq.detectable_colors(cores)
-
-def mapeia_verde(sensor):                                                     #
-    dados = sensor.hsv()                                                      #
-    if (160 <= dados.h <= 200) and (dados.s > 25) and (40 <= dados.v <= 100): # função ler verde
-        return True                                                           #
-    return False                                                              #
-
-omnitrix = StopWatch()
-
-omnitrix.reset()
-andar.straight(40)
-print(omnitrix.time())
+Color.BLACK = Color(h=240, s=100, v=50)
+Color.WHITE = Color(h=0, s=0, v=100)
+Color.GREEN = Color(h=186, s=80, v=50)
+Color.RED = Color(h=0, s=100, v=50)
+cores_ler = (Color.SILVER, Color.BLACK, Color.WHITE, Color.GREEN, Color.RED)
+cores.detectable_colors(cores_ler)
 
 while True:
-    esq_e_verde = mapeia_verde(coresq)
-    dir_e_verde = mapeia_verde(cordir)
-    dist = ultra.distance()
-    esq = coresq.color()
-    dir = cordir.color()
-    meio = cormeio.reflection()
-    arfagem, rolagem = hub.imu.tilt()
-    arfagem = arfagem + 3.6
-    hsv_esq = coresq.hsv()
-    hsv_meio = cormeio.hsv()
-    hsv_dir = cordir.hsv() 
+    cor = cores.color()
+    hsv = cores.hsv()
+    print("cor:", cor, "| h:", hsv.h, "s:", hsv.s, "v:", hsv.v)
     wait(20)
-    guinada = hub.imu.heading()
-    if esq_e_verde:
-        hub.light.on(Color.GREEN)
-    else:
-        hub.light.on(Color.RED)
